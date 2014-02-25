76d06a9fb472bbeffb91c434c91e82fdd6c00a7b
k_somemo <kntsmy@hotmail.co.jp>
2014-02-25 13:06:03 +0900

    Update lone_sum.py

--- Logic-2/lone_sum.py
+++ Logic-2/lone_sum.py
@@ -9,3 +9,11 @@ def lone_sum(a, b, c):
         return b
     else:
         return a + b + c        
+# sample
+#def lone_sum(a, b, c):
+#  sum = 0
+#  if a != b and a != c: sum += a
+#  if b != a and b != c: sum += b
+#  if c != a and c != b: sum += c
+#  
+#  return sum
