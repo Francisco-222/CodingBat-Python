dd1afbfaf8f0f3f1b2c15d516909f45cafe08829
k_somemo <kntsmy@hotmail.co.jp>
2014-02-25 14:59:21 +0900

    Update double_char.py

--- String-2/double_char.py
+++ String-2/double_char.py
@@ -1,4 +1,2 @@
 def double_char(str):
-    if len(str) == 0:
-        return str
-    return reduce(lambda x,y: x + y, map(lambda x: x * 2, list(str)))
+    return ''.join(map(lambda x: x * 2, list(str)))
