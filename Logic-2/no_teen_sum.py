0563be19ffb596859cfa3bcd58d0e4eb826c76df
k_somemo <kntsmy@hotmail.co.jp>
2014-02-25 13:45:00 +0900

    Update no_teen_sum.py

--- Logic-2/no_teen_sum.py
+++ Logic-2/no_teen_sum.py
@@ -1,2 +1,5 @@
 def no_teen_sum(a, b, c):
-    return sum(map(lambda x: 0 if (13 <= x <= 14 or 17 <= x <= 19) else x, [a, b, c]))
+    return sum(map(fix_teen, [a, b, c]))
+
+def fix_teen(x):
+    return 0 if (13 <= x <= 14 or 17 <= x <= 19) else x
